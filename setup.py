4417a1102c4c353b7f976e94b2621d00deeb4a05
Teemu Husso <teemu.husso@gmail.com>
2011-10-12 18:57:40 +0300

    Add basic files
    Install for development with
    
    $ python setup.py develop

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='treecomments',
+    version='0.01',
+    description='Django tree-like comments',
+    author='Teemu Husso',
+    author_email='teemu.husso@gmail.com',
+    url='',
+    packages=find_packages(exclude=['ez_setup']),
+    package_dir={
+        '': 'src',
+        },
+    install_requires=[
+        'setuptools',
+        ],
+)
