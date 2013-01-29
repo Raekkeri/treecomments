ed602fc434fd803bc3ef0636d0b65f1d8a60851d
Jere Malinen <jere.malinen@arcusys.fi>
2013-01-29 11:55:26 +0200

    Fix setup.py for installing by pip

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='treecomments',
     author='Teemu Husso',
     author_email='teemu.husso@gmail.com',
     url='',
-    packages=find_packages(exclude=['ez_setup']),
+    packages=find_packages('src', exclude=['ez_setup']),
     package_dir={
         '': 'src',
         },
