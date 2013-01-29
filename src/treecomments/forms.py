f0ea998dd6e24ecfb60c9020a9cd203eab64a73f
Jere Malinen <jere.malinen@arcusys.fi>
2013-01-29 13:02:43 +0200

    Set email field optional

--- src/treecomments/forms.py
+++ src/treecomments/forms.py
@@ -1,12 +1,19 @@
 import logging
 
 from django import forms
-
 from django.contrib.comments.forms import CommentForm
+from django.conf import settings
+
+
+COMMENT_EMAIL_REQUIRED = getattr(settings, "COMMENT_EMAIL_REQUIRED", False)
 
 
 class TreeCommentForm(CommentForm):
     parent_pk = forms.IntegerField(widget=forms.HiddenInput, required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(TreeCommentForm, self).__init__(*args, **kwargs)
+        self.fields['email'].required = COMMENT_EMAIL_REQUIRED
 
     def get_comment_create_data(self):
         data = super(TreeCommentForm, self).get_comment_create_data()
